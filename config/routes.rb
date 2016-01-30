28e8885c67a1d3a94af916de2983c4564ed22c1b
wkodate <n9lfcth9@gmail.com>
2016-01-30 19:33:22 +0900

    create static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-
   get 'static_pages/help'
+  get 'static_pages/about'
 
   resources :items
   resources :rsses
@@ -9,7 +8,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'rsses#index'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
