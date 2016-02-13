f44c6c4964d80c998e4549fab7a5177d5a4ec1cf
wkodate <n9lfcth9@gmail.com>
2016-02-14 02:45:43 +0900

    inish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
 
   resources :items
   resources :rsses
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
