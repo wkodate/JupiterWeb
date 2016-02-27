1059297ac978c9e8327a11e9d2bea6caa193610b
wkodate <n9lfcth9@gmail.com>
2016-02-27 19:45:02 +0900

    Add password resests & email configuration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160222113310) do
+ActiveRecord::Schema.define(version: 20160227091551) do
 
   create_table "items", force: :cascade do |t|
     t.string   "link"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 20160222113310) do
     t.string   "activation_digest"
     t.boolean  "activated",         default: false
     t.datetime "activated_at"
+    t.string   "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
