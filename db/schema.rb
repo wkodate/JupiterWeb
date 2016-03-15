d2383f40004c6c9d67fd77f9bf0ffd7d22674d69
wkodate <n9lfcth9@gmail.com>
2016-03-15 23:50:06 +0900

    change the column on items table #9

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160312042855) do
+ActiveRecord::Schema.define(version: 20160313125539) do
 
   create_table "items", force: :cascade do |t|
     t.string   "link",        limit: 255
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20160312042855) do
     t.text     "description", limit: 65535
     t.datetime "date"
     t.string   "image",       limit: 255
-    t.string   "rss_url",     limit: 255
+    t.integer  "rss_id",      limit: 4
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
   end
