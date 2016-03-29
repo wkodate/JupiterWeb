c1ba0ab8f416f05793f98fbb89c0858a7331e561
wkodate <n9lfcth9@gmail.com>
2016-03-29 20:02:58 +0900

    add rss site link #7

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ActiveRecord::Base
+  delegate :title, to: :rss, prefix: true, allow_nil: true
+  delegate :site_link, to: :rss, prefix: true, allow_nil: true
   belongs_to :rss
   validates :link, presence: true
   validates :title, presence: true
