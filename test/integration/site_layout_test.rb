3cacb9ac90e8d65cb2a6858420faa0097db36391
wkodate <n9lfcth9@gmail.com>
2016-03-30 23:47:25 +0900

    fix layout of header #12

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -3,10 +3,9 @@ require 'test_helper'
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   test "layout links" do
-    get home_path
-    assert_template 'static_pages/home'
+    get about_path
+    assert_template 'static_pages/about'
     assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
   end
