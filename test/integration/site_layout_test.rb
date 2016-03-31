23dfec0ad796fcc67092aaf9df84bb50695899d9
wkodate <n9lfcth9@gmail.com>
2016-03-31 22:16:29 +0900

    fix test code #15

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,9 +5,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get about_path
     assert_template 'static_pages/about'
-    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", root_path, count: 3
     assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", "https://twitter.com/matome_nanj"
   end
 
 end
