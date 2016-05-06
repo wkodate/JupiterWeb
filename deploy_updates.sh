1f7e4616d42a60143044d435fbdb0ab102318633
wkodate <n9lfcth9@gmail.com>
2016-05-06 22:10:28 +0900

    fix bug

--- deploy_updates.sh
+++ deploy_updates.sh
@@ -2,6 +2,6 @@
 
 cd /var/www/html/jupiter-web/ &&
 git pull origin master &&
-bundle install --deployment --without development test &&
+bundle install --without development test &&
 bundle exec rake assets:precompile db:migrate RAILS_ENV=production &&
 passenger-config restart-app
