d1dc20403ca2e39202ce9fd687740ba61b57efec
wkodate <hbwandeow@gmail.com>
2019-04-18 01:34:22 +0900

    fix almost all test code

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,6 +37,8 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  # Default Mailer Host
+  Rails.application.routes.default_url_options[:host] = 'localhost'
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
@@ -44,6 +46,4 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  # Default Mailer Host
-  #Rails.application.routes.default_url_options[:host] = 'localhost'
 end
