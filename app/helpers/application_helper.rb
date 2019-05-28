0a419214f21fdae625d9ca524ff28fca439ad706
wkodate <hbwandeow@gmail.com>
2019-05-28 22:12:21 +0900

    replace url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,15 +2,15 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: 'なんJまとめのまとめ',
-      keywords: "なんJまとめのまとめ,なんJ,アンテナ,野球",
+      keywords: "なんJまとめのまとめ,なんJ,アンテナ,野球,高校野球,5ch,2ch",
       description: "なんでも実況(ジュピター)まとめブログのアンテナサイトです。プロ野球好きにはたまらない野球情報が盛りだくさん！数あるなんJまとめサイトを最新記事からまとめて閲覧することができます。野球,NPB,MLB,なんJ,おんJネタが好きな方にはおすすめ！",
       icon: "favicon.ico",
       author: "https://twitter.com/hbwandeow",
-      canonical: "https://matome-nanj.net",
+      canonical: "https://matome-nanj.herokuapp.com",
       og: {
         title:    'なんJまとめのまとめ',
         type:     'website',
-        url:      'https://matome-nanj.net',
+        url:      'https://matome-nanj.herokuapp.com',
         image:    'favicon.ico',
       }
     }
